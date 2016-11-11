2b5f686f66f25efcf14b10c8f45bef0eb8900244
Tu <tu.nguyenanh312@gmail.com>
2016-11-11 13:22:35 +0700

    .

--- src/main/java/uet/controller/UserController.java
+++ src/main/java/uet/controller/UserController.java
@@ -1,8 +1,14 @@
 package uet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import uet.model.User;
 import uet.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Tu on 11-Nov-16.
@@ -11,4 +17,20 @@ import uet.service.UserService;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    //signup
+    @RequestMapping(value="/signup",method = RequestMethod.POST)
+    public User signup(@RequestBody User user){
+        return userService.signup(user);
+    }
+    //login
+    @RequestMapping(value="login", method = RequestMethod.POST)
+    public User login(@RequestBody User user) {
+        return userService.login(user);
+    }
+    //logout
+    @RequestMapping(value="/logout", method = RequestMethod.POST)
+    public User logout(@RequestBody User user){
+        return userService.logout(user);
+    }
 }
--- src/main/java/uet/service/UserService.java
+++ src/main/java/uet/service/UserService.java
@@ -1,7 +1,45 @@
 package uet.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import uet.model.User;
+import uet.repository.UserRepository;
+
 /**
  * Created by Tu on 11-Nov-16.
  */
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public User signup(User userDTO){
+        User user1  = userRepository.findByUserName(userDTO.getUsername());
+        if (user1 == null){
+            if(userDTO.getUsername() != null && userDTO.getPassword() != null){
+                User user = new User();
+                user.setUsername(userDTO.getUsername());
+                user.setPassword(userDTO.getPassword());
+                user.setStatus(0);
+                return userRepository.save(user);
+            } else {
+                throw new NullPointerException("Missing information.");
+            }
+        } else {
+            throw new NullPointerException("User existed.");
+        }
+    }
+
+    public User login(User userDTO){
+        User user = userRepository.findByUserName(userDTO.getUsername());
+        if (userDTO.getPassword().equals(user.getPassword())){
+            user.setStatus(1);
+            return userRepository.save(user);
+        } else {
+            throw new NullPointerException("Wrong password.");
+        }
+    }
+
+    public User logout(User user){
+        user.setStatus(0);
+        return userRepository.save(user);
+    }
 }
